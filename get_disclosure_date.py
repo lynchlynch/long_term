b37e66f0c1b9279cbc9babef8beb0a5b4f70b3f0
lynchlynch <hitcas_gy@163.com>
2020-10-15 08:39:49 +0800

    dd

--- get_disclosure_date.py
+++ get_disclosure_date.py
@@ -1,6 +1,8 @@
 import tushare as ts
 import os
 import pandas as pd
+from tqdm import tqdm
+import numba as nba
 
 disclosure_result_path = 'D:/pydir/Raw Data/Tushare_pro/disclosure_date/'
 
@@ -12,7 +14,7 @@ year_list = [str(i) for i in year_list]
 end_quater_list = ['0331','0630','0930','1231']
 for single_year in year_list:
     for single_quater in end_quater_list:
-        print(str(single_year) + '-----------' + str(single_quater))
+        # print(str(single_year) + '-----------' + str(single_quater))
         end_date = single_year + single_quater
         df = pro.disclosure_date(end_date = end_date)
         df.to_csv(disclosure_result_path + end_date + '.csv',index=False)
@@ -29,10 +31,23 @@ for single_file in disclosure_file_list:
     single_disclosure_data = pd.read_csv(disclosure_result_path + single_file)
     total_disclosure_df = total_disclosure_df.append(single_disclosure_data)
 
+total_disclosure_df = total_disclosure_df.reset_index(drop=True)
+
+total_disclosure_df.to_csv(disclosure_result_path + 'total_disclosure_111.csv',index=False)
+
 code_list = []
-for index in range(len(total_disclosure_df)):
+for index in tqdm(range(len(total_disclosure_df)),desc='index'):
     code = int((total_disclosure_df['ts_code'].tolist()[index]).split('.')[0])
     code_list.append(code)
+    pre_date = total_disclosure_df.loc[index,'pre_date']
+    if len(str(pre_date)) < 5:#处理空值的情况
+        pre_date = 0xfff
 
+    actual_date = total_disclosure_df.loc[index, 'actual_date']
+    if len(str(actual_date)) < 5:#处理空值的情况
+        actual_date = 0xfff
+
+    total_disclosure_df.loc[index,'pre_date'] = int(pre_date)
+    total_disclosure_df.loc[index, 'actual_date'] = int(actual_date)
 total_disclosure_df['stock_code'] = code_list
 total_disclosure_df.to_csv(disclosure_result_path + 'total_disclosure.csv',index=False)
--- verify_doctor_tao_parallel.py
+++ verify_doctor_tao_parallel.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
     rps_N3 = 250
     high_price_threshold = 0.9
     # rps_threshold_list = [85, 85, 85]
-    rps_threshold_list = [70, 70, 70]
+    rps_threshold_list = [80, 80, 80]
     result_path = result_path + str(rps_threshold_list[0]) + '/'
 
     duration_month = 8

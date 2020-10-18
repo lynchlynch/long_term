933dbeeae720e6f258dec64147044244785a3bad
lynchlynch <hitcas_gy@163.com>
2020-10-18 21:55:00 +0800

    dd

--- verify_doctor_tao_parallel.py
+++ verify_doctor_tao_parallel.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
     rps_N3 = 250
     high_price_threshold = 0.9
     # rps_threshold_list = [85, 85, 85]
-    rps_threshold_list = [75, 75,  75]
+    rps_threshold_list = [70, 70,  70]
     result_path = result_path + str(rps_threshold_list[0]) + '/'
 
     duration_month = 8

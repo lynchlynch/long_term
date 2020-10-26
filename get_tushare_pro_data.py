54dd170c86783da65f94a39b770d52b9011123d9
lynchlynch <yepei1988@163.com>
2020-10-26 16:03:28 +0800

    mac

--- get_tushare_pro_data.py
+++ get_tushare_pro_data.py
@@ -1,22 +1,23 @@
 #由于Tushare数据放在私人服务器中，如果一次获取较多数据，容易被误认为是DDos攻击，故分组获取
 import tushare as ts
 import time
+import pandas as pd
+import os
 from tqdm import tqdm
 
 ts.set_token('d34c57b1749de7df7766a60bb615078696e935856d99575c6b91bb5d')
 pro = ts.pro_api('d34c57b1749de7df7766a60bb615078696e935856d99575c6b91bb5d')
-stock_code = pro.query(exchange='', list_status='L', fields='ts_code')['ts_code']
-
+stock_code = pro.stock_basic(exchange='', list_status='L', fields='ts_code')['ts_code']
 leap = 5#设置每组股票数
 groups = list(range(0,len(stock_code)//leap + 1))#分组
 no_data_stocks = []#储存无数据股票代码，以便重新下载
 download_failed_stocks = []
-# root_path = '/Users/pei/PycharmProjects/Raw Data/Tushare_pro/daily_data/'
-root_path = 'D:/pydir/Raw Data/Tushare_pro/daily_data/'
+root_path = '/Users/pei/PycharmProjects/Raw Data/Tushare_pro/daily_data/'
+# root_path = 'D:/pydir/Raw Data/Tushare_pro/daily_data/'
 
 # start_date = '20171009'
 start_date = '20121009'
-current_day = '20200306'
+current_day = '20201026'
 
 start_time = time.time()
 #分组下载数据

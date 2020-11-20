81ff12c567b7abef4e7995cac331b23552b35e8e
lynchlynch <hitcas_gy@163.com>
2020-11-20 20:58:49 +0800

    dd

--- draw_total_k_line.py
+++ draw_total_k_line.py
@@ -1,4 +1,13 @@
 import pandas as pd
 import os
 
+daily_stock_path = 'D:/pydir/Raw Data/Tushare_pro/daily_data/'
+# stock_path = '/Users/pei/PycharmProjects/Raw Data/Tushare_pro/daily_data/'
+report_path = 'D:/pydir/Raw Data/Report/PerReport'
+# report_path = '/Users/pei/PycharmProjects/Raw Data/Report/PerReport'
+result_path = 'D:/pydir/long_term/veri_result/veri_doctor_tao/'
+# result_path = '/Users/pei/PycharmProjects/docter_tao/result'
+weekly_stock_path = 'D:/pydir/Raw Data/Tushare_pro/weekly_data/'
+# weekly_stock_path = '/Users/pei/PycharmProjects/Raw Data/Tushare_pro/weekly_data/'
+
 

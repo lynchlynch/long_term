ea7c51210bb480a76c0d7783d8594d9bafb612a3
lynchlynch <yepei1988@163.com>
2020-11-23 15:38:50 +0800

    mac

--- draw_total_k_line.py
+++ draw_total_k_line.py
@@ -6,14 +6,16 @@ from tqdm import tqdm
 
 start_time = time.time()
 
-daily_stock_path = 'D:/pydir/Raw Data/Tushare_pro/daily_data/'
-# stock_path = '/Users/pei/PycharmProjects/Raw Data/Tushare_pro/daily_data/'
-report_path = 'D:/pydir/Raw Data/Report/PerReport'
-# report_path = '/Users/pei/PycharmProjects/Raw Data/Report/PerReport'
-result_path = 'D:/pydir/long_term/veri_result/veri_doctor_tao/'
-# result_path = '/Users/pei/PycharmProjects/docter_tao/result'
-weekly_stock_path = 'D:/pydir/Raw Data/Tushare_pro/weekly_data/'
-# weekly_stock_path = '/Users/pei/PycharmProjects/Raw Data/Tushare_pro/weekly_data/'
+# daily_stock_path = 'D:/pydir/Raw Data/Tushare_pro/daily_data/'
+daily_stock_path = '/Users/pei/PycharmProjects/Raw Data/Tushare_pro/daily_data/'
+# weekly_stock_path = 'D:/pydir/Raw Data/Tushare_pro/weekly_data/'
+weekly_stock_path = '/Users/pei/PycharmProjects/Raw Data/Tushare_pro/weekly_data/'
+# report_path = 'D:/pydir/Raw Data/Report/PerReport'
+report_path = '/Users/pei/PycharmProjects/Raw Data/Report/PerReport'
+# result_path = 'D:/pydir/long_term/veri_result/veri_doctor_tao/'
+result_path = '/Users/pei/PycharmProjects/docter_tao/result'
+# weekly_stock_path = 'D:/pydir/Raw Data/Tushare_pro/weekly_data/'
+weekly_stock_path = '/Users/pei/PycharmProjects/Raw Data/Tushare_pro/weekly_data/'
 
 rps = 85
 period_pre_daily = 500
@@ -30,9 +32,12 @@ for index in tqdm(range(len(to_draw_file_df)),desc='index'):
     single_stock_code = to_draw_file_df['stock_code'].tolist()[index]
     single_stock_code = zeroize.zeroize(single_stock_code)
     single_buy_date = int(to_draw_file_df['buy_date'].tolist()[index])
+    ##画日K线
     draw_k_line_fuc.draw_k_line(daily_stock_path,result_path + 'raw_tao/daily/',single_stock_code,
                                 single_buy_date,period_pre_daily,period_post_daily)
-
+    ##画周K线
+    draw_k_line_fuc.draw_k_line(daily_stock_path, result_path + 'raw_tao/weekly/', single_stock_code,
+                                single_buy_date, period_pre_daily, period_post_daily)
 
 
 end_time = time.time()
